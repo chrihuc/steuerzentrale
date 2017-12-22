fe3a97ae7341651aa54110008344fb1c60422f98
Christoph Huckle <chrihuc@gmail.com>
2017-12-22 22:16:16 +0100

    fixed

--- tifo/settings.py
+++ tifo/settings.py
@@ -19,13 +19,13 @@ outputs = {'Vm1ZIM1RUM1DO01':'IO16o',#'V01ZIM1RUM1DO02':'IO16o','V01ZIM1RUM1DO03
 
 # generel setup of IO16
 # tifoadress:(inputa,inputb,outputa,outputb,monoflopa,monoflopb,floptime[ms],normally open a, b)
-IO16 = {'62efV1.gox':(0b00111111,0b00000000,0b00000000,0b00000000,0b00000000,0b00000000,5000,0b00000000,0b00000000),
-        '6QGwm1.vYN':(0b00000000,0b00000000,0b00000000,0b00001111,0b00000000,0b00001111,5000,0b00000000,0b00000000)}
+IO16 = {'62efV1.gox':(0b00111111,0b00000000,0b00000000,0b00000000,0b00000000,0b00000000,500,0b00000000,0b00000000),
+        '6QGwm1.vYN':(0b00000000,0b00000000,0b00000000,0b11110000,0b00000000,0b11110000,500,0b00000000,0b00000000)}
 # assingment of homecontrol addresses to tifo addreses
 IO16i = {'62efV1.gox':{'a0b1':'V00WOH1SRA1DI01','a0b10':'V00WOH1SRA1DI02','a0b100':'V00WOH1SRA1DI03',
                        'a0b1000':'V00WOH1SRA1DI04','a0b10000':'V00WOH1SRA1DI05','a0b100000':'V00WOH1SRA1DI06'}}
-IO16o = {'Vm1ZIM1RUM1DO01':({'Value':0,'Commands':({'UID':'6QGwm1.vYN','Pin':0b00000001,'Port':'B','Value':1})},
-                            {'Value':1,'Commands':({'UID':'6QGwm1.vYN','Pin':0b00000010,'Port':'B','Value':1})}),
+IO16o = {'Vm1ZIM1RUM1DO01':({'Value':0,'Commands':({'UID':'6QGwm1.vYN','Pin':0b01000000,'Port':'B','Value':1})},
+                            {'Value':1,'Commands':({'UID':'6QGwm1.vYN','Pin':0b10000000,'Port':'B','Value':1})}),
 #         'Vm1ZIM1RUM1DO01':({'Value':0,'Commands':({'UID':'63mHZj.vYN','Pin':0b00000001,'Port':'B','Value':1},
 #                                                   {'UID':'63mHZj.vYN','Pin':0b00000010,'Port':'B','Value':0})},
 #                            {'Value':1,'Commands':({'UID':'63mHZj.vYN','Pin':0b00000001,'Port':'B','Value':0},
