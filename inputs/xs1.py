6f8e914b434b3f2df47812d4ade45ed375f1777a
Christoph Huckle <chrihuc@gmail.com>
2018-04-07 20:39:55 +0200

    xs1 test

--- inputs/xs1.py
+++ inputs/xs1.py
@@ -24,12 +24,13 @@ selfsupervision = True
 aes = aevs.AES()
 conn = pycurl.Curl()
 last_data = ""
-ezcontrol = xs1.XS1(constants.xs1_.IP) 
+ezcontrol = xs1.XS1(constants.xs1_.IP)
+conn = pycurl.Curl()
 #scenes = szenen.Szenen()
 
 def last_data_reset():
     last_data = ""
-            
+
 def on_receive(data):
     if not constants.run:
         sys.exit("Error message")
@@ -39,7 +40,7 @@ def on_receive(data):
     if False:
         ldt.cancel()
         ldt = Timer(1, last_data_reset)
-        ldt.start() 
+        ldt.start()
         if last_data == data:
             return
     count = int(msqc.setting_r("NumRestart"))
@@ -52,7 +53,7 @@ def on_receive(data):
     toolbox.log(data)
     value = float(data.split(" ")[-1])
     name = 'XS1.' + str(data.split(" ")[-3])
-    #now = datetime.datetime.now().strftime("%H:%M:%S.%f") 
+    #now = datetime.datetime.now().strftime("%H:%M:%S.%f")
     #aes.new_event(description="Empfangen: " + name + " " + str(now), prio=0)
 #####################
 #Heartbeat & Server Steuerung
@@ -60,7 +61,7 @@ def on_receive(data):
     if (("heartbeat" in name) and (value == 0)):
         heartbeat.cancel()
         heartbeat = Timer(constants.heartbt, heartbeat_sup)
-        heartbeat.start()   
+        heartbeat.start()
         ezcontrol.SetSwitchFunction("heartbeat", "1")
     szenen.Szenen.trigger_scenes(name, value)
 #    szns, desc = msqc.inputs(name,value)
@@ -68,11 +69,11 @@ def on_receive(data):
 #        if szene <> None:
 #            scenes.threadExecute(szene, check_bedingung=False, wert=value, device=desc)
     last_data = data
-    
+
 def heartbeat_sup():
   if selfsupervision:
     zeit =  time.time()
-    now = (strftime("%Y-%m-%d %H:%M:%S",localtime(zeit)))  
+    now = (strftime("%Y-%m-%d %H:%M:%S",localtime(zeit)))
     msqc.setting_s("Autorestart", str(now))
     count = int(msqc.setting_r("NumRestart"))
     if count == 0:
@@ -84,36 +85,39 @@ def heartbeat_sup():
     exectext = "sudo killall python"
     print "XS1 connection lost"
     if toolbox.ping(constants.router_IP):
-        os.system(exectext)
+        conn.close()
+#        sys.exit("XS1 goodbye")
+        #os.system(exectext)
     else:
         reset_wlan()
-        os.system(exectext)   
+        sys.exit("XS1 goodbye")
+        #os.system(exectext)
 
 def reset_wlan():
-    os.system('sudo ifdown --force wlan0') 
+    os.system('sudo ifdown --force wlan0')
     os.system('sudo ifup wlan0')
 
 
-def main():  
+def main():
     global heartbeat
     global ldt
     zeit =  time.time()
-    now = (strftime("%Y-%m-%d %H:%M:%S",localtime(zeit)))  
+    now = (strftime("%Y-%m-%d %H:%M:%S",localtime(zeit)))
     msqc.setting_s("Laststart", str(now))
     if selfsupervision:
         while not toolbox.ping(constants.router_IP):
             #reset_wlan()
-            time.sleep(60)        
+            time.sleep(60)
     heartbeat = Timer(constants.heartbt, heartbeat_sup)
     heartbeat.start()
-    ezcontrol.SetSwitchFunction("heartbeat", "1") 
+    ezcontrol.SetSwitchFunction("heartbeat", "1")
     ldt = Timer(2, last_data_reset)
-    conn = pycurl.Curl()  
-    conn.setopt(pycurl.URL, constants.xs1_.STREAM_URL)  
+
+    conn.setopt(pycurl.URL, constants.xs1_.STREAM_URL)
     conn.setopt(pycurl.WRITEFUNCTION, on_receive)
     #aes.new_event(description="XS1inputs neugestartet", prio=0)
     conn.perform()
 
-    
+
 if __name__ == '__main__':
-    main()    
+    main()
