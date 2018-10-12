bff627ea633791b6234a24db08b460dfc569cfed
Christoph Huckle <chrihuc@gmail.com>
2018-10-12 19:55:54 +0200

    fixes

--- inputs/mqtt_client.py
+++ inputs/mqtt_client.py
@@ -87,14 +87,14 @@ def on_message(client, userdata, msg):
                 szene = msg.topic.split('/')[2]
                 broadcast_exec_szn(szene)
             if 'Device' in msg.topic:
-#                device = msg.topic.split('/')[2]
+
                 device = m_in['Device']
                 command = m_in['Command']
                 broadcast_exec_comm(device, command)
         elif 'AlarmOk' in msg.topic:
-            aes.alarm_liste.delAlarm(m_in['uuid'])
-#            name = msg.topic[7:]
-#            broadcast_input_value('MQTT.' + name, float(m_in['Value']))
+            if 'uuid' in m_in.keys:
+                aes.alarm_liste.delAlarm(m_in['uuid'])
+
     except ValueError:
         print("no json code")
 
