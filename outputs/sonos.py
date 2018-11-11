4645401d122c78f0a132fb5e1826a143601dc576
Christoph Huckle <chrihuc@gmail.com>
2018-11-11 14:20:49 +0100

    p3 fix

--- outputs/sonos.py
+++ outputs/sonos.py
@@ -575,7 +575,7 @@ class Sonos:
     def soco_set_status(self,player):
         dicti = self.Status[player.player_name]
         tries = 0
-        while (dicti <> self.soco_get_status(player, store=False)) and tries < 7:
+        while (dicti != self.soco_get_status(player, store=False)) and tries < 7:
             tries += 1
             try:
                 player_ip = player.ip_address
