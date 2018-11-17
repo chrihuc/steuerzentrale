5a0777dc143b266f61f13f945b733d1994f325b9
Christoph Huckle <chrihuc@gmail.com>
2018-11-17 13:37:58 +0100

    fixing

--- inputs/mqtt_client.py
+++ inputs/mqtt_client.py
@@ -78,8 +78,9 @@ def dis_con (*args, **kargs):
 
 def on_message(client, userdata, msg):
 #    print(msg.topic + " " + str(msg.payload))
+    message = str(msg.payload.decode("utf-8"))
     try:
-        m_in=(json.loads(msg.payload.decode("utf-8"))) #decode json data
+        m_in=(json.loads(message)) #decode json data
         print(m_in)
         if 'Inputs' in msg.topic:
             name = msg.topic[7:]
@@ -100,7 +101,7 @@ def on_message(client, userdata, msg):
             if 'Wecker' in msg.topic:
                 mqtt_pub("DataRequest/Answer/Wecker", crn.get_all(wecker=True))
     except ValueError:
-        print("no json code")
+        print("no json code", message)
 
 mqtt.Client.connected_flag=False
 client = None
