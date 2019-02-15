de50156a5849dd7664782746f0d69f78ce0c6ee2
Christoph Huckle <chrihuc@gmail.com>
2019-02-15 05:55:13 +0100

    fix bracket too many

--- database/mysql_connector.py
+++ database/mysql_connector.py
@@ -630,7 +630,7 @@ def inputs(device, value, add_to_mqtt=True):
     #            sql2 = sql2 + ' AND (Gradient_lt > "' + str(gradient) + '" OR Gradient_lt is NULL )'
     #            sql2 = sql2 + ' AND (Gradient_gt < "' + str(gradient) + '" OR Gradient_gt is NULL )'
                 sql2 = sql + ' AND (enabled = "True" OR enabled is NULL )'
-                sql2 = sql2 + ');'
+                sql2 = sql2 + ';'
                 cur.execute(sql + sql2)
                 results = cur.fetchall()
                 field_names = [i[0] for i in cur.description]
